e8e800886fcce16027fd15c00e1929583f591809
Franz Casabuena <franzneandra@gmail.com>
2019-10-07 12:32:21 +0800

    will deploy after this

---
+++ build/precache-manifest.d58bd30594c4cc9e511b30c57d420c2d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "03c3b94019f3a17c8c21",
+    "url": "/lights-out/static/css/main.e7f5a0c0.chunk.css"
+  },
+  {
+    "revision": "03c3b94019f3a17c8c21",
+    "url": "/lights-out/static/js/main.03c3b940.chunk.js"
+  },
+  {
+    "revision": "498adbd33ac33c4b9d54",
+    "url": "/lights-out/static/js/1.498adbd3.chunk.js"
+  },
+  {
+    "revision": "5cc5b55a4753d848057b",
+    "url": "/lights-out/static/js/runtime~main.5cc5b55a.js"
+  },
+  {
+    "revision": "8d289fa72c76fab0b6aafc19e7ff991f",
+    "url": "/lights-out/index.html"
+  }
+];
